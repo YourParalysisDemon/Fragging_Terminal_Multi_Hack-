f0ad3fd98355c1f45f1f99ec2c8e36eb016109a1
YourParalysisDemon <145605048+YourParalysisDemon@users.noreply.github.com>
2025-01-06 17:29:37 -0500

    Update Pycho.py
    Moved offsets and added colorama.

--- Pycho.py
+++ Pycho.py
@@ -4,6 +4,7 @@ import pygame
 import time
 import pymem.exception
 import webbrowser
+import colorama
 from threading import Thread
 from pymem import *
 from pymem.process import *
@@ -11,47 +12,27 @@ from pymem.ptypes import RemotePointer
 from time import sleep
 from time import *
 from tkinter import ttk
+from colorama import Fore, Back, Style
+from offsets import *
+
+colorama.init()
+
 
 while True:
-    password = input("Enter password ")
+    password = input(Fore.RED + Back.BLACK + Style.BRIGHT + "Enter password ")
     if password == "115":
-        print("Welcome")
+        print(Fore.RED + Back.BLACK + Style.BRIGHT + "Welcome")
         break
     else:
-        print("Try again retard")
+        print(Fore.RED + Back.BLACK + Style.BRIGHT + "Try again retard")
 
-game = input("\nEnter game: ")
+game = input(Fore.RED + Back.BLACK + Style.BRIGHT + "\nEnter game: ")
 if game == "1":
     mem = Pymem("Psychonauts")
     module1 = module_from_name(mem.process_handle, "Psychonauts.exe").lpBaseOfDll
 elif game == "2":
     mem = Pymem("Psychonauts2-Win64-Shipping")
     module = module_from_name(mem.process_handle, "Psychonauts2-Win64-Shipping.exe").lpBaseOfDll
-
-# Pycho 1 offsets
-health1_offsets = [0X0, 0X4, 0X0, 0X100, 0X10, 0X0, 0X2C8]
-gravity1_offsets = [0X0, 0X94]
-blast_offsets = [0X4, 0X18, 0XC7, 0X253C]
-run_offsets = [0X4, 0X708]
-no_clip_offsets = [0X0, 0X110]
-# no_clip_offsets = [0X0, 0X170] 0x00386AE0
-walk_offsets = [0X120]  # 003839D8
-z_offsets = [0X10, 0X44]  # 003839D8
-y_offsets = [0X0, 0X40]  # 00386AE0
-x_offsets = [0X10, 0X48]  # 003839D8
-
-# Pycho 2 offsets
-laser_offsets = [0x8, 0x8, 0x270, 0xE0, 0x8, 0x104]
-health_offsets = [0x0, 0x20, 0x568, 0xD0, 0x0, 0x30]
-speed_offsets = [0xE0, 0x90, 0xD0, 0x500, 0x0, 0x0, 0xAC]
-cash_offsets = [0x490, 0x140, 0x68, 0x3E8, 0x230]
-pspops_offsets = [0x490, 0x140, 0x128, 0x3E8, 0x180]
-player_speed = [0x20, 0x120, 0x98]
-gravity_offsets2 = [0x90, 0x360, 0x2b8, 0xb50]
-walk_vel_offsets = [0X8, 0X8, 0X190, 0X19C]
-max_vel_offsets = [0X8, 0X8, 0X190, 0X1B0]
-flip_gravity_offsets = [0X8, 0X8, 0X190, 0X148]
-clock_offsets = [0XE8, 0XE0, 0X328]
 
 
 def getpointeraddress(base, offsets):
