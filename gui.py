6a2b25bac36d7faa58bedac2bee03d64125af109
YourParalysisDemon <145605048+YourParalysisDemon@users.noreply.github.com>
2025-01-12 20:29:02 -0500

    Update gui.py
    Added halo 1

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ pygame.mixer_music.load("music/mod.mp3")
 pygame.mixer_music.play(1)
 
 root = tk.Tk()
-photo = tk.PhotoImage(file="back/155.png")
+photo = tk.PhotoImage(file="back/snake.png")
 root.wm_iconphoto(False, photo)
 root.attributes("-topmost", True)
 root.title("Fragging Terminal")
@@ -43,6 +43,8 @@ tab1 = ttk.Frame(notebook)
 tab2 = ttk.Frame(notebook)
 tab3 = ttk.Frame(notebook)
 tab4 = ttk.Frame(notebook)
+tab5 = ttk.Frame(notebook)
+tab6 = ttk.Frame(notebook)
 
 
 # tabs to the notebook
@@ -50,9 +52,11 @@ notebook.add(tab1, text='Psychonauts 1')
 notebook.add(tab2, text='Psychonauts 2')
 notebook.add(tab3, text='Dev/Info')
 notebook.add(tab4, text='BioShock Infinite')
+notebook.add(tab5, text='Halo 1 new')
+notebook.add(tab6, text='Halo 1 old')
 
 
-# first tab
+# first tab pycho 1
 button1_tab1 = tk.Button(tab1, text="Health", bg='black', fg='white', cursor="cross", command=multi_run_god)
 button1_tab1.pack(pady=10)
 
@@ -74,7 +78,7 @@ button5_tab1.pack(pady=10)
 button6_tab1 = tk.Button(tab1, text="Exit", bg='black', fg='white', cursor="cross", command=root.destroy)
 button6_tab1.pack(pady=10)
 
-# second tab
+# second tab pycho 2
 button1_tab2 = tk.Button(tab2, text="Health", bg='black', fg='white', cursor="cross", command=multi_run_god2)
 button1_tab2.pack(pady=10)
 
@@ -97,11 +101,11 @@ button6_tab2.pack(pady=10)
 button7_tab2 = tk.Button(tab2, text="Exit", bg='black', fg='white', cursor="cross", command=root.destroy)
 button7_tab2.pack(pady=10)
 
-# three tab
+# three tab dev/info
 label1_tab4 = tk.Label(tab3, text="Hello and thank you for using my software.")
 label1_tab4.pack(pady=10)
 
-# tab four
+# tab four bioshock
 button1_tab4 = tk.Button(tab4, text="Pistol Ammo", bg='black', fg='white', cursor="cross", command=multi_bio_pistol)
 button1_tab4.pack(pady=10)
 
@@ -111,6 +115,60 @@ button2_tab4.pack(pady=10)
 button3_tab4 = tk.Button(tab4, text="Fuck Gravity", bg='black', fg='white', cursor="cross",
                          command=multi_bio_fly)
 button3_tab4.pack(pady=10)
+
+# tabs for halo 1
+
+button1_tab5 = tk.Button(tab5, text="Health", bg='black', fg='white', cursor="cross", command=multi_run_new_health)
+button1_tab5.pack(pady=10)
+
+button2_tab5 = tk.Button(tab5, text="UNSC Fire rate", bg='black', fg='white', cursor="cross", command=multi_run_117)
+button2_tab5.pack(pady=10)
+
+button3_tab5 = tk.Button(tab5, text="Covenant Fire rate", bg='black', fg='white', cursor="cross",
+                         command=multi_run_plasma)
+button3_tab5.pack(pady=10)
+
+button4_tab5 = tk.Button(tab5, text="Bullet Pierce", bg='black', fg='white', cursor="cross",
+                         command=multi_run_wall_pierce)
+button4_tab5.pack(pady=10)
+
+button5_tab5 = tk.Button(tab5, text="No Spread", bg='black', fg='white', cursor="cross", command=multi_run_nospread)
+button5_tab5.pack(pady=10)
+
+button6_tab5 = tk.Button(tab5, text="Shotgun", bg='black', fg='white', cursor="cross", command=multi_run_shotgun)
+button6_tab5.pack(pady=10)
+
+button7_tab5 = tk.Button(tab5, text="Throw Hands", bg='black', fg='white', cursor="cross", command=multi_run_hands)
+button7_tab5.pack(pady=10)
+
+button8_tab5 = tk.Button(tab5, text="No Clip", bg='black', fg='white', cursor="cross", command=multi_run_clip)
+button8_tab5.pack(pady=10)
+
+button9_tab5 = tk.Button(tab5, text="Speed", bg='black', fg='white', cursor="cross", command=multi_run_speed)
+button9_tab5.pack(pady=10)
+
+button10_tab5 = tk.Button(tab5, text="Plasma Pistol", bg='black', fg='white', cursor="cross",
+                          command=multi_run_plasma_pistol)
+button10_tab5.pack(pady=10)
+
+# second tab
+button1_tab6 = tk.Button(tab6, text="Health", bg='black', fg='white', cursor="cross", command=multi_run_old_health)
+button1_tab6.pack(pady=10)
+
+button2_tab6 = tk.Button(tab6, text="UNSC Fire rate", bg='black', fg='white', cursor="cross", command=multi_run_0ld_117)
+button2_tab6.pack(pady=10)
+
+button3_tab6 = tk.Button(tab6, text="Speed", bg='black', fg='white', cursor="cross", command=multi_run_speed)
+button3_tab6.pack(pady=10)
+
+button4_tab6 = tk.Button(tab6, text="Confuse NPC", bg='black', fg='white', cursor="cross", command=multi_run_haha)
+button4_tab6.pack(pady=10)
+
+button5_tab6 = tk.Button(tab6, text="Pause", bg='black', fg='white', cursor="cross", command=multi_run_pause)
+button5_tab6.pack(pady=10)
+
+button5_tab6 = tk.Button(tab6, text="Stats", bg='black', fg='white', cursor="cross", command=multi_run_stats)
+button5_tab6.pack(pady=10)
 
 # Clock
 time_label = tk.Label(tab3, font=("Arial", 10), fg="Black", bg="Red")
@@ -150,6 +208,7 @@ keyboard.add_hotkey("1", tele_main_lodge)
 keyboard.add_hotkey("k", root.destroy)
 keyboard.add_hotkey("l", multi_run_spam)
 keyboard.add_hotkey("F", multi_run_fuck_gravity)
+keyboard.add_hotkey("F", multi_bio_fly)
 keyboard.add_hotkey("Z", multi_run_flip)
 keyboard.add_hotkey("-", multi_run_slow)
 keyboard.add_hotkey("+", multi_run_fast)
