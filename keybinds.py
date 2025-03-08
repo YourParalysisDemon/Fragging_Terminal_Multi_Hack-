45fb3b4fb5bf36a2ce053c8646bc5859b57807a7
YourParalysisDemon <145605048+YourParalysisDemon@users.noreply.github.com>
2025-03-08 18:00:46 -0500

    Create keybinds.py

---
+++ keybinds.py
@@ -0,0 +1,17 @@
+from Pycho1 import *
+
+keyboard.add_hotkey("Z", multi_run_flip)
+keyboard.add_hotkey("-", multi_run_slow)
+keyboard.add_hotkey("+", multi_run_fast)
+keyboard.add_hotkey("9", multi_run_stop)
+keyboard.add_hotkey("l", multi_run_spam)
+keyboard.add_hotkey("F", multi_run_fuck_gravity)
+keyboard.add_hotkey("G", multi_run_walls)
+keyboard.add_hotkey("F", multi_run_gravity)
+keyboard.add_hotkey("1", tele_main_lodge)
+keyboard.add_hotkey("5", multi_run_117)
+keyboard.add_hotkey("6", multi_run_plasma)
+keyboard.add_hotkey("V", multi_run_clip)
+keyboard.add_hotkey("G", multi_run_gravity)
+keyboard.add_hotkey("F", spongebob_multi_fly)
+keyboard.add_hotkey("F", multi_bio_fly)
