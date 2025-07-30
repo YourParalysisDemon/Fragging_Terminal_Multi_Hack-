2510046b1a4b22130ff88835040ae067a8721e06
YourParalysisDemon <145605048+YourParalysisDemon@users.noreply.github.com>
2025-07-29 22:00:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,26 +17,19 @@ from time import *
 from tkinter import ttk
 from colorama import Fore, Back, Style
 from offsets import *
-from gui import *
 
 colorama.init()
 
-while True:
-    password = input(Fore.RED + Back.BLACK + Style.BRIGHT + "Enter password ")
-    if password == "115":
-        print(Fore.RED + Back.BLACK + Style.BRIGHT + "Welcome")
-        break
-    else:
-        print(Fore.RED + Back.BLACK + Style.BRIGHT + "Try again retard")
-
-game_list = ["Games Available",
+game_list = [Fore.RED + Back.BLACK + Style.BRIGHT + "Games Available",
              "● Psychonauts",
              "● Psychonauts 2",
              "● Halo 1",
              "● Bioshock infinite",
              "● The binding of isaac",
              "● Spongebob CosmicShake",
-             "● Metro 2033"]
+             "● Metro 2033",
+             "● Company of Heroes"]
+
 for game_list in game_list:
     print(game_list)
 
@@ -67,7 +60,7 @@ while True:
         module_isaac = module_from_name(mem.process_handle, "isaac-ng.exe").lpBaseOfDll
         print("Game Found!")
         break
-    elif game == "TF2":  # Do not attach to a VAC secured server retard. Only run this code in -insecure mode.  
+    elif game == "TF2":  # Do not attach to a VAC secured server retard. Only run this code in -insecure mode.
         mem = Pymem("tf_win64")
         module_tf2 = module_from_name(mem.process_handle, "engine.dll").lpBaseOfDll
         print("Game Found!")
@@ -85,6 +78,11 @@ while True:
     elif game == "Fallout 3":
         mem = Pymem("Fallout3")
         module_fallout = module_from_name(mem.process_handle, "Fallout3.exe").lpBaseOfDll
+        print("Game Found!")
+        break
+    elif game == "Company of Heroes":
+        mem = Pymem("RelicCOH")
+        module = module_from_name(mem.process_handle, "WW2Mod.dll").lpBaseOfDll
         print("Game Found!")
         break
     else:
@@ -117,172 +115,3 @@ def getpointeraddress(base, offsets):
             return remote_pointer.value + offset
 
 
-# Threads for bioshock inf
-
-def multi_bio_fly():
-    new_thread = Thread(target=bio_fly, daemon=True)
-    new_thread.start()
-
-
-def multi_bio_mon():
-    new_thread = Thread(target=bio_mon, daemon=True)
-    new_thread.start()
-
-
-def multi_bio_pistol():
-    new_thread = Thread(target=bio_pistol, daemon=True)
-    new_thread.start()
-
-
-# Isaac threads
-def multi_isaac_health():
-    new_thread = Thread(target=isaac_health, daemon=True)
-    new_thread.start()
-
-
-def multi_isaac_bomb():
-    new_thread = Thread(target=isaac_bombs, daemon=True)
-    new_thread.start()
-
-
-def multi_isaac_fire():
-    new_thread = Thread(target=isaac_fire_rate, daemon=True)
-    new_thread.start()
-
-
-# CosmicShake-Win64-Shipping.exe threads
-
-def spongebob_multi_fly():
-    new_thread = Thread(target=sponge_fly, daemon=True)
-    new_thread.start()
-
-
-# TF2 threads
-
-def multi_tf2():
-    new_thread = Thread(target=stats_tf2, daemon=True)
-    new_thread.start()
-
-
-def stats_tf2():  # Just a little experiment :) still heavily a wip. Will not work in VAC secured servers so don't be stupid. 
-    addr1 = readpointeraddress(module_tf2 + 0x006DA9F8, health_tf2_offsets_1)
-    addr2 = readpointeraddress(module_tf2 + 0x00479848, health_tf2_offsets_2)
-    addr3 = readpointeraddress(module_tf2 + 0x0072FD18, health_tf2_offsets_3)
-    addr4 = readpointeraddress(module_tf2 + 0x00779CD8, player_name_1)
-    addr5 = readpointeraddress(module_tf2 + 0x00479868, player_name_2)
-    addr6 = readpointeraddress(module_tf2 + 0x0053E6E8, player_name_3)
-    while 1:
-        try:
-            h1 = mem.read_short(addr1)
-            h2 = mem.read_short(addr2)
-            h3 = mem.read_short(addr3)
-            p1 = mem.read_string(addr4)
-            p2 = mem.read_string(addr5)
-            p3 = mem.read_string(addr6)
-            R = [p1, h1,
-                 p2, h2,
-                 p3, h3]
-            print("\r", R, end="", flush=True)
-        except pymem.exception.MemoryReadError as e:
-            print(f"Error reading memory: {e}")
-            break
-        if keyboard.is_pressed("F1"):
-            break
-
-
-# funcs for bioshock inf
-
-def bio_fly():
-    addr1 = getpointeraddress(module_bio + 0x00358660, fly_offs)
-    while 1:
-        try:
-            mem.write_int(addr1, 0x43fa0000)
-        except pymem.exception.MemoryWriteError as e:
-            print(f"Error writing memory: {e}")
-            break
-        if keyboard.is_pressed("F1"):
-            mem.write_int(addr1, 0x0)
-            break
-
-
-def bio_pistol():
-    addr1 = getpointeraddress(module_bio + 0x00F6648C, pistol_ammo_offs)
-    addr2 = getpointeraddress(module_bio + 0x00F687F8, salt_offs)
-    while 1:
-        try:
-            mem.write_int(addr1, 0x0000000c)
-            mem.write_int(addr2, 0x00000064)
-        except pymem.exception.MemoryWriteError as e:
-            print(f"Error writing memory: {e}")
-            break
-        if keyboard.is_pressed("F1"):
-            mem.write_int(addr1, 0x0000000c)
-            mem.write_int(addr2, 0x0000000c)
-            break
-
-
-def bio_mon():
-    addr1 = getpointeraddress(module_bio + 0x00FA2B98, money_offs)
-    while 1:
-        try:
-            mem.write_int(addr1, 0x00002328)
-        except pymem.exception.MemoryWriteError as e:
-            print(f"Error writing memory: {e}")
-            break
-        if keyboard.is_pressed("F1"):
-            mem.write_int(addr1, 0x00002328)
-            break
-
-
-# Isaac functions needs to be fixed :(
-
-def isaac_health():
-    addr = getpointeraddress(module_isaac + 0x0021E3FC, isaac_health_offsets)
-    while 1:
-        try:
-            mem.write_int(addr, 0x00000006)
-        except pymem.exception.MemoryWriteError as e:
-            print(f"Error writing memory: {e}")
-            break
-        if keyboard.is_pressed("F1"):
-            break
-
-
-def isaac_bombs():
-    addr = getpointeraddress(module_isaac + 0x0021E508, bomb_offsets)
-    addr2 = getpointeraddress(module_isaac + 0x0021E598, bomb_timer_offsets)
-    while 1:
-        try:
-            mem.write_int(addr, 0x5)
-            mem.write_int(addr2, 0x0)
-        except pymem.exception.MemoryWriteError as e:
-            print(f"Error writing memory: {e}")
-            break
-        if keyboard.is_pressed("F1"):
-            break
-
-
-def isaac_fire_rate():
-    addr = getpointeraddress(module_isaac + 0x0021A1F4, isaac_fire_rate_offsets)
-    while 1:
-        try:
-            mem.write_int(addr, 0x0)
-        except pymem.exception.MemoryWriteError as e:
-            print(f"Error writing memory: {e}")
-            break
-        if keyboard.is_pressed("F1"):
-            break
-
-
-# Spongebob CosmicShake
-
-def sponge_fly():
-    addr = getpointeraddress(module_sponge + 0x057309F0, spongebob_fly)
-    while 1:
-        try:
-            mem.write_int(addr, 0x43fa0000)
-        except pymem.exception.MemoryWriteError as e:
-            print(f"Error writing memory: {e}")
-            break
-        if keyboard.is_pressed("c"):
-            break
