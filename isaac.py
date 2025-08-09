dc7be6aa2d7e0b12a800aa4aa42b25c3ea276164
YourParalysisDemon <145605048+YourParalysisDemon@users.noreply.github.com>
2025-08-09 13:48:59 -0400

    Update isaac.py

--- isaac.py
+++ isaac.py
@@ -1,5 +1,5 @@
-from offsets import *
 from main import *
+from offsets import *
 
 
 def getpointeraddress(base, offsets):
@@ -16,6 +16,11 @@ def multi_isaac_health():
     new_thread.start()
 
 
+def multi_isaac_health_set():
+    new_thread = Thread(target=isaac_health_set, daemon=True)
+    new_thread.start()
+
+
 def multi_isaac_bomb():
     new_thread = Thread(target=isaac_bombs, daemon=True)
     new_thread.start()
@@ -29,15 +34,25 @@ def multi_isaac_fire():
 # Isaac functions needs to be fixed :(
 
 def isaac_health():
-    addr = getpointeraddress(module_isaac + 0x0021E3FC, isaac_health_offsets)
+    addr = getpointeraddress(module_isaac + 0x007FD65C, isaac_health_offsets)
     while 1:
         try:
-            mem.write_int(addr, 0x00000006)
+            mem.write_int(addr, 0x41200000)
         except pymem.exception.MemoryWriteError as e:
             print(f"Error writing memory: {e}")
             break
         if keyboard.is_pressed("F1"):
             break
+
+
+def isaac_health_set():
+    addr1 = getpointeraddress(module_isaac + 0x007FD65C, isaac_health_offsets)
+    try:
+        health_input = float(input("\nEnter health value: "))
+        print("\nHealth set")
+        mem.write_float(addr1, health_input)
+    except pymem.exception.MemoryWriteError as e:
+        print(f"Error writing memory: {e}")
 
 
 def isaac_bombs():
@@ -55,13 +70,12 @@ def isaac_bombs():
 
 
 def isaac_fire_rate():
-    addr = getpointeraddress(module_isaac + 0x0021A1F4, isaac_fire_rate_offsets)
+    addr = getpointeraddress(module_isaac + 0x007FD65C, isaac_fire_rate_offsets)
     while 1:
         try:
-            mem.write_int(addr, 0x0)
+            mem.write_double(addr, float(0))
         except pymem.exception.MemoryWriteError as e:
             print(f"Error writing memory: {e}")
             break
         if keyboard.is_pressed("F1"):
             break
-
