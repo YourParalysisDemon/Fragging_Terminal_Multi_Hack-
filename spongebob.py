c692bd04cf311acea92f94d887165af72121aa1f
YourParalysisDemon <145605048+YourParalysisDemon@users.noreply.github.com>
2025-10-04 10:41:28 -0400

    Update spongebob.py
    Added Garfield Kart here cause i don't feel like making its very own script.

--- spongebob.py
+++ spongebob.py
@@ -8,6 +8,16 @@ def spongebob_multi_fly():
     new_thread.start()
 
 
+def garfield_rich():
+    new_thread = Thread(target=garfield_cash_func, daemon=True)
+    new_thread.start()
+
+
+def garfield_fast():
+    new_thread = Thread(target=garfield_speed_func, daemon=True)
+    new_thread.start()
+
+
 def getpointeraddress(base, offsets):
     remote_pointer = RemotePointer(mem.process_handle, base)
     for offset in offsets:
@@ -26,6 +36,42 @@ def sponge_fly():
             print(f"Error writing memory: {e}")
             break
         if keyboard.is_pressed("c"):
+            break
+
+
+def garfield_cash_func():
+    addr = getpointeraddress(module_garfield + 0x001F10AC, garfield_cash)
+    while 1:
+        try:
+            mem.write_int(addr, 0x100000)
+        except pymem.exception.MemoryWriteError as e:
+            print(f"Error writing memory: {e}")
+            break
+        if keyboard.is_pressed("c"):
+            break
+
+
+def garfield_speed_func():
+    addr = getpointeraddress(module_garfield + 0x001F3944, garfield_speed)
+    while True:
+        try:
+            speed_input = float(input("\nSet speed: "))
+            go = input("\nReady for lasagna? (yes/no): ")
+            if go == "yes":
+                print("It's time for lasagna!!!")
+                while 1:
+                    try:
+                        mem.write_float(addr, speed_input)
+                    except pymem.exception.MemoryWriteError as e:
+                        print(f"Error writing memory: {e}")
+                    if keyboard.is_pressed("c"):
+                        break
+            else:
+                if go == "no":
+                    print("Lasagna going in the fridge")
+                    break
+        except pymem.exception.MemoryWriteError as e:
+            print(f"Error writing memory: {e}")
             break
 
 
